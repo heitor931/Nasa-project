9d7dbfec8697793c805ec73d03cbf2178c987cee
Engineer <heitorino1@gmail.com>
2022-09-23 05:20:38 +0100

    More fixes to app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
+const path = require("path");
 const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
-const path = require("path");
 const morgan = require("morgan");
 
 const api = require("./routes/api");
